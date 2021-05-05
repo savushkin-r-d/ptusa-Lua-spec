77e272162fa1f3f11d9ae2f6acb45a6794617bfe
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2021-05-05 10:32:34 +0300

    Add base test script

---
+++ main.lua
@@ -0,0 +1,50 @@
+require 'busted.runner'()
+
+expose("ptusa", function()
+    ptusa = require ( 'main_control' )
+
+    it("load", function()
+        assert.is_truthy(package.loaded.main_control)    --module is not loaded
+        assert.is_truthy( ptusa )
+
+        if no_print_stack_traceback then
+            ptusa.no_print_stack_traceback()
+        end
+    end)
+
+    it("init", function()
+        local res = ptusa.init(
+            main_script or "main.plua",
+            "path", path or "",
+            "sys_path", sys_path or "./sys-scripts/" )
+
+        assert.are.equal(res, 0)
+
+        print( user_init )
+    end)
+
+    it("has at least one object", function()
+        assert.is_truthy( OBJECTS[1] )
+    end)
+
+    it("eval() function", function()
+        assert.are.equal( ptusa.eval(), 0 )
+    end)
+
+    it("objects operations", function()
+        for idx, obj in pairs(OBJECTS) do
+            local name = obj.name
+            print( idx, name, obj.n )
+            local operations_cnt = obj:get_modes_count()
+            for i = 1, operations_cnt do
+                print( "    ", i )
+
+                obj:set_mode( i, 1 )
+                assert.are.equal( ptusa.eval(), 0 )
+
+                obj:set_mode( i, 0 )
+                assert.are.equal( ptusa.eval(), 0 )
+            end
+        end
+    end)
+end)
