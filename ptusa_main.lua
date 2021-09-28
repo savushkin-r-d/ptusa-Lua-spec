2e149f0fb093803d476a8888786b5d69539cca07
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2021-09-28 11:22:03 +0300

    Add script to test locally
    Use this script from git submodule

---
+++ ptusa_main.lua
@@ -0,0 +1,58 @@
+package.path = package.path..
+    ";p:/PTUSA-projects/editor/lua/busted/?.lua"..
+    ";p:/PTUSA-projects/editor/lua/5.1/lua/?/init/init.lua"..
+    ";p:/PTUSA-projects/editor/lua/5.1/lua/?/init.lua"..
+    ";p:/PTUSA-projects/editor/lua/5.1/lua/?.lua"
+
+package.cpath = package.cpath ..
+    ";p:/PTUSA-projects/editor/lua/5.1/clibs/?.dll"..
+    ";./bin/lib/?.dll"
+
+require 'busted.runner'()
+
+expose("ptusa", function()
+    ptusa = require ( 'ptusa_main' )
+
+    it("load", function()
+        assert.is_truthy(package.loaded.ptusa_main)    --module is not loaded
+        assert.is_truthy( ptusa )
+
+        if no_print_stack_traceback then
+            ptusa.no_print_stack_traceback()
+        end
+    end)
+
+    it("init", function()
+        local res = ptusa.init(
+            main_script or "main.plua",
+            "path", path or "",
+            "sys_path", sys_path or "./sys/" )
+
+        assert.are.equal(res, 0)
+    end)
+
+    it("has at least one object", function()
+        assert.is_truthy( OBJECTS[1] )
+    end)
+
+    it("eval() function", function()
+        assert.are.equal( ptusa.eval(), 0 )
+    end)
+
+    it("objects operations", function()
+        for idx, obj in pairs(OBJECTS) do
+            local name = obj.name
+            print( idx, name, obj.n )
+            local operations_cnt = obj:get_modes_count()
+            for i = 1, operations_cnt do
+                print( "    ", i )
+
+                obj:set_mode( i, 1 )
+                assert.are.equal( ptusa.eval(), 0 )
+
+                obj:set_mode( i, 0 )
+                assert.are.equal( ptusa.eval(), 0 )
+            end
+        end
+    end)
+end)
