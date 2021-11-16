adf9ef3f8fa0fd26a852ca00a83c13554259a7bf
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2021-11-16 10:47:09 +0300

    Check if objects exist

--- ptusa_main.lua
+++ ptusa_main.lua
@@ -21,27 +21,25 @@ expose("ptusa", function()
         assert.are.equal(res, 0)
     end)
 
-    it("has at least one object", function()
-        assert.is_truthy( OBJECTS[1] )
-    end)
-
     it("eval() function", function()
         assert.are.equal( ptusa.eval(), 0 )
     end)
 
     it("objects operations", function()
-        for idx, obj in pairs(OBJECTS) do
-            local name = obj.name
-            print( idx, name, obj.n )
-            local operations_cnt = obj:get_modes_count()
-            for i = 1, operations_cnt do
-                print( "    ", i )
+        if OBJECTS then
+            for idx, obj in pairs(OBJECTS) do
+                local name = obj.name
+                print( idx, name, obj.n )
+                local operations_cnt = obj:get_modes_count()
+                for i = 1, operations_cnt do
+                    print( "    ", i )
 
-                obj:set_mode( i, 1 )
-                assert.are.equal( ptusa.eval(), 0 )
+                    obj:set_mode( i, 1 )
+                    assert.are.equal( ptusa.eval(), 0 )
 
-                obj:set_mode( i, 0 )
-                assert.are.equal( ptusa.eval(), 0 )
+                    obj:set_mode( i, 0 )
+                    assert.are.equal( ptusa.eval(), 0 )
+                end
             end
         end
     end)
